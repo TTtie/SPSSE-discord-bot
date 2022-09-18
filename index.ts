c5191131624be33abc23fbc2a7197bf96013f8de
Petr Mácha <info@mapetr.moe>
2022-09-18 23:59:11 +0200

    Prepare for adding leveling system

--- index.ts
+++ index.ts
@@ -4,9 +4,11 @@ import "@sentry/tracing"
 import * as path from "path";
 import * as fs from "fs";
 import dotenv from "dotenv";
+import Realm from "realm";
 
 dotenv.config();
 
+// Setup for Sentry
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
@@ -14,11 +16,14 @@ Sentry.init({
   environment: process.env.NODE_ENV,
 });
 
+// Discord.js Client
 const client = new Client({ intents: 34511 });
 
+// Load all events
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
+// Attach events to client
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
   // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -42,4 +47,36 @@ for (const file of eventFiles) {
   }
 }
 
-client.login(process.env.TOKEN);
+declare global {
+  let realm: Realm
+}
+
+// XP Schema for realm database
+const xpSchema = {
+  name: "xp",
+  properties: {
+    _id: "int",
+    userId: "int",
+    name: "string",
+    xp: "int",
+  },
+  primaryKey: "_id",
+}
+
+// Connect to realm database
+Realm.open({
+  path: "realm",
+  schema: [xpSchema]
+})
+  .then((r) => realm = r)
+  .catch(err => {
+    console.error(err);
+    Sentry.captureException(err);
+  });
+
+// Login to Discord
+client.login(process.env.TOKEN)
+  .catch(err => {
+    console.error(err);
+    Sentry.captureException(err);
+  });
