025fa3eacfc49d31ea26c46afb0d144e65203dd1
TTtie <me@tttie.cz>
2022-10-01 18:37:14 +0000

    do not read config synchronously

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -1,20 +1,22 @@
 import {Message, PermissionsBitField} from "discord.js";
 import {captureException} from "@sentry/node";
-import * as fs from "fs";
+import * as fsPromises from "fs/promises";
 
 export default {
   name: 'messageCreate',
   once: false,
   async execute(message: Message) {
     if (message.author.bot) return;
-    const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+    const config = JSON.parse(await fsPromises.readFile('./config.json', 'utf8'));
     switch (message.content) {
       case '#setChannel':
         if (!message.member?.permissions.has(PermissionsBitField.Flags.ManageGuild)) break;
         config.channel = message.channelId;
-        fs.writeFile('./config.json', JSON.stringify(config), (err) => {
-          if (err) captureException(err);
-        });
+        try {
+          await fsPromises.writeFile('./config.json', config);
+        } catch(err) {
+          captureException(err);
+        }
         await message.reply('Channel set!').then((message: Message) => {
           setTimeout(() => message.delete(), 5000);
         });
